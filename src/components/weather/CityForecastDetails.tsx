1652d8a502c71e18a0997b3226fbbb96bba3754e
robinhlgrsn <robinholgersson91@gmail.com>
2021-06-16 20:20:06 +0200

    Temporary filtering for current date

--- src/components/weather/CityForecastDetails.tsx
+++ src/components/weather/CityForecastDetails.tsx
@@ -1,10 +1,6 @@
 import React, { useContext, useEffect } from 'react'
-import { Container, Paper, TableContainer, Table, TableHead, TableCell, TableRow, TableBody, Hidden } from '@material-ui/core'
+import { makeStyles, Container, TableContainer, Table, TableHead, TableCell, TableRow, TableBody, Hidden } from '@material-ui/core'
 import WeatherContext from '../../context/weather-context'
-
-
-
-
 
 
 const array = [
@@ -18,9 +14,14 @@ const array = [
     coordinates: `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${16.158}/lat/${60.5812}/data.json`
   },
 ]
+const useStyles = makeStyles({
+  table: {
+    maxWidth: 800,
+  }
+})
 
 const CityForecastDetails = () => {
-
+  const classes = useStyles();
   const ctx = useContext(WeatherContext)
   const loading = ctx.isLoading
 
@@ -30,63 +31,57 @@ const CityForecastDetails = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+
+  let filteredWeatherData;
   if (ctx.weatherData.length > 0) {
-    console.log(ctx.weatherData)
+    const currentDate = new Date()
+    filteredWeatherData = ctx.weatherData[0].filter((weather: any) => weather.time.getDate() === currentDate.getDate())
   }
 
-
-
   return (
-    <>
-      <Container style={{ width: "100vw", display: "flex", alignItems: "center", flexDirection: "column" }} maxWidth='lg' disableGutters>
-        {!loading && ctx.weatherData.length > 0 ?
-          <>
-            < h1 > Göteborg</h1>
-            <TableContainer component={Paper} >
-              <Table padding="none" size="small" aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">Tid</TableCell>
-                    <TableCell align="center">Väder</TableCell>
-                    <TableCell align="center">Temp</TableCell>
-                    <TableCell align="center">Nederbörd</TableCell>
-                    <TableCell align="center">Vind</TableCell>
+    <Container style={{ width: "100vw", display: "flex", alignItems: "center", flexDirection: "column" }} maxWidth='lg' disableGutters>
+      {!loading && ctx.weatherData.length > 0 ?
+        <>
+          < h1 > Göteborg</h1>
+          <TableContainer className={classes.table}  >
+            <Table padding="none" size="small" aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Tid</TableCell>
+                  <TableCell align="center">Väder</TableCell>
+                  <TableCell align="center">Temp</TableCell>
+                  <TableCell align="center">Nederbörd</TableCell>
+                  <TableCell align="center">Vind</TableCell>
+                  <Hidden xsDown>
+                    <TableCell align="center">Luftfuktighet</TableCell>
+                  </Hidden>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {filteredWeatherData.map((row: any) => (
+                  <TableRow key={row.id}>
+                    <TableCell align="center" component="th" >
+                      {row.time.getHours()}
+                    </TableCell>
+                    <TableCell align="center" component="th" >
+                      <img width="100" src={require(`../../assets/icons/${row.weatherSymbol}.png`).default} alt="" />
+                    </TableCell>
+                    <TableCell align="center">
+                      {Math.floor(row.temp)}°
+                    </TableCell>
+                    <TableCell align="center">{row.precipitation} mm</TableCell>
+                    <TableCell align="center">{row.windSpeed} m/s</TableCell>
                     <Hidden xsDown>
-                      <TableCell align="center">Luftfuktighet</TableCell>
+                      <TableCell align="center">{row.humidity} %</TableCell>
                     </Hidden>
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {ctx.weatherData[0].map((row: any) => (
-                    <TableRow key={row.id}>
-                      <TableCell align="center" component="th" >
-                        {row.time.getHours()}
-                      </TableCell>
-                      <TableCell align="center" component="th" >
-                        <img src={require(`../../assets/icons/${row.weatherSymbol}.png`).default} width="100" alt="" />
-                      </TableCell>
-                      <TableCell align="center">
-                        {Math.floor(row.temp)}°
-                      </TableCell>
-                      <TableCell align="center">{row.precipitation} mm</TableCell>
-                      <TableCell align="center">{row.windSpeed} m/s</TableCell>
-                      <Hidden xsDown>
-                        <TableCell align="center">{row.humidity} %</TableCell>
-                      </Hidden>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer >
-          </>
-          :
-          <h1>LOADING....</h1>}
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer >
+        </> : <h1>LOADING....</h1>}
+    </Container>
 
-      </Container>
-
-
-
-    </>
   )
 }
 
