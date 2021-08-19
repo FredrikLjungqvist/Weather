d3f2cfffd997ea840683cfe045185f3f36d5f28f
Fredrik Ljungqvist <fljungqvist@gmail.com>
2021-08-19 13:12:04 +0200

    Lining of code

--- src/components/weather/CurrentWeather.tsx
+++ src/components/weather/CurrentWeather.tsx
@@ -44,24 +44,24 @@ export default function CurrentWeather() {
   const classes = useStyles();
   return (
     <>
-              <Link className={classes.link} to={`/stad/${currentPositionInfo[0].city}/datum/${date}`}>
-      <Container className={classes.root}>
-            <Typography gutterBottom variant="h1" component="h2">
-                { ctx.weatherData.length > 0 && localStorage.getItem("positions") === null ? 'Loading': 
-                <div>
-                  {currentPositionInfo[0].city}
-                </div>
-                }
-            </Typography>
-            {ctx.weatherData.length > 0 ? (
-              <Container className={classes.weatherContainer}>
-                <Typography variant="h1" component="h2">
-                  {Math.floor(ctx.weatherData[0][0].temp)}°
-                </Typography>
-                <img width="200" src={require(`../../assets/icons/${ctx.weatherData[0][0].weatherSymbol}.png`).default} alt="" />
-              </Container>)
-            : <CircularProgress />} 
-      </Container>
+      <Link className={classes.link} to={`/stad/${currentPositionInfo[0].city}/datum/${date}`}>
+        <Container className={classes.root}>
+              <Typography gutterBottom variant="h1" component="h2">
+                  { ctx.weatherData.length > 0 && localStorage.getItem("positions") === null ? 'Loading': 
+                  <div>
+                    {currentPositionInfo[0].city}
+                  </div>
+                  }
+              </Typography>
+              {ctx.weatherData.length > 0 ? (
+                <Container className={classes.weatherContainer}>
+                  <Typography variant="h1" component="h2">
+                    {Math.floor(ctx.weatherData[0][0].temp)}°
+                  </Typography>
+                  <img width="200" src={require(`../../assets/icons/${ctx.weatherData[0][0].weatherSymbol}.png`).default} alt="" />
+                </Container>)
+              : <CircularProgress />} 
+        </Container>
       </Link>
     </> 
   );
